9d6b628ac2a9518cbd5d9faac691b523ebdc0ede
Pavel Tvaladze <reddragonsportal@gmail.com>
2025-11-15 00:18:14 +0400

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,14 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
   },
+  build: {
+    rollupOptions: {
+      input: {
+        index1: './index1.html',
+        index2: './index2.html',
+        index3: './index3.html',
+        index4: './index4.html',
+      }
+    },
+  },
 });
